00cef671250f3e358632b5356ac9696a63885cda
JaimeJesúsSerrano <jaime.serranorodriguez@alum.uca.es>
2017-07-15 16:47:27 +0200

    Register form updated

--- src/AppBundle/Form/Type/RegistrationType.php
+++ src/AppBundle/Form/Type/RegistrationType.php
@@ -2,25 +2,25 @@
 namespace AppBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class RegistrationType extends AbstractType
 {	
 	public function buildForm(FormBuilderInterface $builder, array $options)
 	{
-		$builder->add('real_name', 'text');
-		$builder->add('real_surname', 'text');
-		$builder->add(
-				'termsAccepted', 
-				'checkbox', 
-				array('property_path' => 'terms_accepted', 'attr' => array('align_with_widget' => true)));
-		$builder->add('submit_button', 'submit', array(
-				'attr' => array('class' => 'submit'),
-				'label' => "Register",
-				'attr' => [
-					'value' => "registration.submit"
-					]
-		));
+        $builder->add('real_name');
+        $builder->add('real_surname');
+//		$builder->add(
+//            'termsAccepted',
+//            CheckboxType::class,
+//            array('property_path' => 'terms_accepted', 'attr' => array('align_with_widget' => true))
+//        );
+        $builder->add(
+            'submit',
+            SubmitType::class,
+            array('label' => "Register")
+        );
 	}
 
 	public function getParent()
