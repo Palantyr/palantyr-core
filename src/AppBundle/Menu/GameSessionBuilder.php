4b04be76afc7c215837850b18c5a58cd1416fe37
Ubuntu <ubuntu@enviroment.box>
2017-10-31 18:28:13 +0000

    Game session secondary menu working

--- src/AppBundle/Menu/GameSessionBuilder.php
+++ src/AppBundle/Menu/GameSessionBuilder.php
@@ -9,52 +9,52 @@ class GameSessionBuilder implements ContainerAwareInterface
 {
     use ContainerAwareTrait;
 
-	public function mainMenu (FactoryInterface $factory, array $options) {
-		
+	public function mainMenu(FactoryInterface $factory, array $options)
+    {
 		$menu = $factory->createItem('root');
 		$menu->setChildrenAttribute('class', 'nav navbar-nav');
 		
 		return $menu;
 	}
 	
-	public function secondaryMenu (FactoryInterface $factory, array $options) {
-		
+	public function secondaryMenu(FactoryInterface $factory, array $options)
+    {
 		$menu = $factory->createItem('root');
 		$menu->setChildrenAttribute('class', 'nav navbar-nav navbar-right');
-		
+
 		self::settingsSubMenu($menu);
 		
 		return $menu;
 	}
 	
-	private function settingsSubMenu ($menu) {
+	private function settingsSubMenu($menu) {
 		$translator = $this->container->get('translator');
-		
+
 		$menu->addChild('settings',
 			array('label' => $translator->trans('secondary_menu.settings.title')))
-				->setAttribute('dropdown', true)
-				->setAttribute('icon', 'fa fa-user');
-		
+				->setExtra('dropdown', true)
+				->setExtra('icon', 'fa fa-user');
+
 		$game_session_id = self::getRouteParameter('game_session_id');
 		$user = $this->container->get('security.token_storage')->getToken()->getUser();
 		if (self::isOwnerToGameSession($user, $game_session_id)) {
 			$menu['settings']->addChild($translator->trans('game_session.edit.title'),
 					array('uri' => 'javascript:void(0)'))
-				->setAttribute('id', 'game_session_edit_button');
+				->setExtra('id', 'game_session_edit_button');
 			$menu['settings']->addChild($translator->trans('secondary_menu.settings.manage_users.title'),
 					array('uri' => 'javascript:void(0)'))
-				->setAttribute('id', 'manage_users_button');
+				->setExtra('id', 'manage_users_button');
 		}
 		else {
 		    $menu['settings']->addChild($translator->trans('secondary_menu.settings.show_users.title'),
 		        array('uri' => 'javascript:void(0)'))
-		        ->setAttribute('id', 'show_users_button');		    
+		        ->setExtra('id', 'show_users_button');
 		}
-		
+
 		$menu['settings']->addChild($translator->trans('secondary_menu.settings.exit.title'),
 				array('route' => 'game_sessions'))
-				->setAttribute('icon', 'fa fa-edit');
-		
+				->setExtra('icon', 'fa fa-edit');
+
 		return $menu;
 	}
     
